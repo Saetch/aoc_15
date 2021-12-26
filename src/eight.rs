e9bc03e196cecfe97b4a168ddccb9b0b0bfa6a51
Saetch <nils.wriedt@hotmail.com>
2021-12-26 01:33:07 +0100

    finalized excercise 8.1 (seemingly wrong)

--- src/eight.rs
+++ src/eight.rs
@@ -1,17 +1,37 @@
+use std::fs::read_to_string;
+
 pub fn eight(){
+    let mut code_characters = 0;
+    let mut string_characters = 0;
+    let inp = read_to_string("eight_input.txt").expect("Could not read file!");
+    let input = inp.replace(" ", "");
+    for line in input.lines(){
+        let mut last: Option<char> = None;
+        let mut current = 'a' ;
+        for char in line.chars(){
+            current = char;
+            if last.is_none(){
+                string_characters+=1;
+                last = Some(char);
+                continue;
+            }
+            if last.unwrap()== '\\' {
+                match current{
+                    'x' => last = None,
+                    '"' => last = None,
+                    '\\' => last = None,
+                    _ => {string_characters+=1; last = None},
+                }
+            }else{
+                last = Some(char);
+                string_characters+=1;
+            }
+        }
 
-    //JUST A TEST 
-    let mut vec = Vec::new();
 
-    for i in 0..=5{
-        vec.push(i);
+        code_characters+=line.len();
     }
-
-    for i in 0..vec.len(){
-        println!("{} -> 1  ", vec[i]);
-    }
-    vec.insert(0, 24);
-    for i in 0..vec.len(){
-        println!("{} -> ", vec[i]);
-    }
+    println!("Code: {}", code_characters);
+    println!("String: {}", string_characters);
+    println!("{}", code_characters-string_characters);
 }
